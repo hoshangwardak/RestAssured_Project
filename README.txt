08137288ac4fb2e419fafe921bff30fa5ab2db27
hoshangwardak <hoshangwardak@gmail.com>
2021-04-03 01:30:39 -0400

    My second commit for First JUnit5 class introduction. I did two tests asserting numbers and character

--- README.txt
+++ README.txt
@@ -1,2 +1,12 @@
-This is where I am going to take my notes
+Day 1
+
+-   When it comes to assertion in JUnit 5, you'll have to the static import of junit jupiter
+    "import static org.junit.jupiter.api.Assertions.assertEquals;" If you don't want to do this static
+    import everytime just put a * at the end instead of the method.
+
+-   assertEquals has one particular version where you can add the error message at the end
+
+
+
+
 
--- src/test/java/day1/JUnit5_Intro.java
+++ src/test/java/day1/JUnit5_Intro.java
@@ -1,14 +1,29 @@
 package day1;
 
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("First Day of JUnit 5")
 public class JUnit5_Intro {
 
-
+    @DisplayName("Asserting Numbers")
     @Test
-    public void test() {
+    public void test1() {
+
+        assertEquals(1,2, "Failed!"); // This one is going to be failed
+        assertEquals(2,2); // This one is going to be passed
 
     }
 
+    @DisplayName("Asserting Character")
+    @Test
+    public void test2() {
+
+        String name = "Yalcin";
+        assertEquals('Y',name.charAt(0),"Failed, they are not equal");
+
+        assertTrue(name.endsWith("n"),"Failed, named does not end with letter n");
+
+    }
 
 }
